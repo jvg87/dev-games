35c3f86fe17e4f02f28daf74a76a1500d04ff37c
jvg87 <jvg87.dev@gmail.com>
2024-06-28 15:52:00 -0300

    chore: organize folders

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Text, View } from "react-native";
+
+const RootLayout = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>teste</Text>
+    </View>
+  );
+};
+
+export default RootLayout;
