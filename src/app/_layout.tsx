f727e51cfa8d62a076f1cd823ad0eb85341630f8
jvg87 <jvg87.dev@gmail.com>
2024-06-29 12:56:45 -0300

    feat: add tanstackQuery setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,18 @@
 import "@/styles/global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+const queryClient = new QueryClient();
+
 const RootLayout = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <StatusBar style="light" />
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </QueryClientProvider>
   );
 };
 
